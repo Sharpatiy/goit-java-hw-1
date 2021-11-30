f08a6cb49ac78f2272a78d93c02f17656459a184
Александр Шарпатый <95072094+Sharpatiy@users.noreply.github.com>
2021-11-30 16:46:02 +0200

    Update HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -1,6 +1,6 @@
 class HelloJava {
     public static void main(String[] args) {
-        System.out.print("Hello");
-        System.out.print("Java");
+        System.out.println("HelloJava");
+        
     }
 }
